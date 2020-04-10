e40a5bcc9d3cb3c9ca9aa565a5442d76df8b4c76
jimothydoe001 <60560345+jimothydoe001@users.noreply.github.com>
2020-04-10 02:40:19 +0200

    almost finished everything

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import requests, sys, urllib
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import json
+import time
+import os
 
+allowedchars = '-.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 ua = UserAgent()
 headers = {'User-Agent': ua.random}
 
@@ -108,10 +112,53 @@ def getTrack(URL, contenttype):
         epartists.append(song_author.text)
 
     return [epsongs, epartists]
-
+class YoutubeSearch:
+    def __init__(self, search_terms: str, max_results=None):
+        self.search_terms = search_terms
+        self.max_results = max_results
+        self.videos = self.search()
+    def search(self):
+        encoded_search = urllib.parse.quote(self.search_terms)
+        BASE_URL = "https://www.youtube.com"
+        url = f"{BASE_URL}/results?search_query={encoded_search}&sp=EgIQAQ%253D%253D"
+        response = BeautifulSoup(requests.get(url).text, "html.parser")
+        results = self.parse_html(response)
+        if self.max_results is not None and len(results) > self.max_results:
+            return results[:self.max_results]
+        return results
+    def parse_html(self, soup):
+        results = []
+        for video in soup.select(".yt-uix-tile-link"):
+            if video["href"].startswith("/watch?v="):
+                video_info = {
+                    "tunefind-title": line.strip('\n'),
+                    "title": video["title"],
+                    "link": "https://www.youtube.com" + video["href"]
+                }
+                results.append(video_info)
+        return results
+    def to_dict(self):
+        return self.videos
+    def to_json(self):
+        return json.dumps(self.videos)
+class TestSearch:
+    def test_init_defaults(self):
+        search = YoutubeSearch('test')
+        assert search.max_results is None
+        assert 1 <= len(search.videos)
+    def test_init_max_results(self):
+        search = YoutubeSearch('test', max_results=10)
+        assert 10 == search.max_results
+        assert 10 == len(search.videos)
+    def test_dict(self):
+        search = YoutubeSearch('test', max_results=10)
+        assert isinstance(search.to_dict(), list)
+    def test_json(self):
+        search = YoutubeSearch('test', max_results=10)
+        assert isinstance(search.to_json(), str)
+        
 URL = input("Please enter TuneFind URL of a TV show or movie: ")
 print("This may take a while...")
-
 
 contenttype = URL.split('/')[3]
 
@@ -124,14 +171,49 @@ temp = removeDuplicates(result[0], result[1])
 songs = temp[1]
 artists = temp[0]
 
-file=open(URL.split('/')[4] + ".txt", "w")
+listfile=open("tmp/files/" + URL.split('/')[4] + ".list.txt", "w")
 
 print("Got all songs from " + URL.split('/')[4].replace("-", " ").title() + " and saved them in '" + URL.split('/')[4] + ".txt'\n")
 for i in range(len(songs)):
     try:
         print("{} - {}".format(songs[i], artists[i]))
-        file.write("{} - {}".format(songs[i], artists[i]) + "\n")
+        listfile.write("{} - {}".format(songs[i], artists[i]) + "\n")
     except IndexError:
         print("Skipped {} - {}".format(songs[i], artists[i]))
-file.close()
-print("Finished.")
+listfile.close()
+print("\n\nFinished retreiving the list, now getting the youtube links.")
+
+
+ytlistfile=open("tmp/files/" + URL.split('/')[4] + ".yt.txt", "w")
+emptystringy = '[]'
+with open('tmp/files/' + URL.split('/')[4] + ".list.txt") as ytlistfile2:
+    for line in ytlistfile2:
+        results = YoutubeSearch(line, max_results=1).to_json()
+        while results == emptystringy:
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+            if results == emptystringy:
+                results = YoutubeSearch(line, max_results=1).to_json()
+                print("\n\nSkipped " + line)
+            break
+        print("\n" + line.strip('\n'))
+        ytlistfile.write(results.strip("[]") + "\n")
+        print(results)
+ytlistfile.close()
+
+with open('tmp/files/' + URL.split('/')[4] + '.yt.txt') as f:
+    for fofo in f:
+        z = json.loads(fofo)
+        newtitle = z["tunefind-title"]
+        all_ascii = ''.join( c for c in newtitle if c in allowedchars )
+        os.system(("mkdir -p {}").format(URL.split('/')[4]))
+        os.system(("youtube-dl -o tmp/{}/'{}.%(ext)s' --cookies tmp/files/cookies.txt -4 --restrict-filenames -x --audio-format mp3 '{}' ").format(URL.split('/')[4], all_ascii, z["link"]))
+f.close()
